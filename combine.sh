5a2b4da64490830b7d43187e1e918c1cae1947bf
Brandon Lee <blee78@gmail.com>
2019-02-23 17:14:00 -0800

    Fixes

---
+++ combine.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cat dlfile.part0 dlfile.part1 dlfile.part2 dlfile.part3 dlfile.part4 dlfile.part5 \
+dlfile.part6 dlfile.part7 dlfile.part8 dlfile.part9 > movie.mkv 
+# dlfile.part10 \
+# dlfile.part11 dlfile.part12 dlfile.part13 dlfile.part14 dlfile.part15 dlfile.part16 \
+# dlfile.part17 dlfile.part18 dlfile.part19 \
+# > movie.mkv
+#cp *.mkv /media/storage/videos/
